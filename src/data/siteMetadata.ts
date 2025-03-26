766c442781a6b3ca098c5f32aa8fcd0a1f9a707b
Jose Ramon Hoz Torre <jose-ramon.hoz-torre@siemens.com>
2025-03-26 21:08:32 +0100

    Feat: Add newsletter and dates

--- src/data/siteMetadata.ts
+++ src/data/siteMetadata.ts
@@ -8,19 +8,6 @@ const siteMetadata = {
   theme: 'system', // system, dark or light
   siteUrl: 'https://automated-aquarium.vercel.app/',
   siteRepo: 'https://github.com/Joserra13/automated-aquarium-web',
-  // set to true if you want a navbar fixed to the top
-  stickyNav: false,
-  newsletter: {
-    // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus, beehive
-    // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
-  },
-  search: {
-    provider: 'kbar', // kbar or algolia
-    kbarConfig: {
-      searchDocumentsPath: `${process.env.BASE_PATH || ''}/search.json`, // path to load documents to search
-    },
-  },
 }
 
 export default siteMetadata
