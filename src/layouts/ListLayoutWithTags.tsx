5bf91d83944f77b22c633347dad110f5be96dc60
Joserra13 <Joserra_013@hotmail.com>
2025-06-15 02:14:18 +0200

    fix(tags): change colors

--- src/layouts/ListLayoutWithTags.tsx
+++ src/layouts/ListLayoutWithTags.tsx
@@ -36,7 +36,7 @@ function Pagination({ totalPages, currentPage }: PaginationProps) {
     <div className="py-8">
       <nav className="flex items-center justify-between">
         {!prevPage && (
-          <button className="px-4 py-2 text-gray-400 border rounded cursor-not-allowed opacity-50" disabled={!prevPage}>
+          <button className="px-4 py-2 text-gray-400 border rounded cursor-not-allowed opacity-50 border-gray-700" disabled={!prevPage}>
             Previous
           </button>
         )}
@@ -44,16 +44,16 @@ function Pagination({ totalPages, currentPage }: PaginationProps) {
           <Link
             href={currentPage - 1 === 1 ? `/${basePath}/` : `/${basePath}/page/${currentPage - 1}`}
             rel="prev"
-            className="px-4 py-2 text-gray-700 bg-white border rounded hover:bg-gray-100 transition-colors duration-200 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:border-gray-700"
+            className="px-4 py-2 border rounded transition-colors duration-200 bg-cyan-800 text-gray-200 hover:bg-cyan-300 border-cyan-500"
           >
             Previous
           </Link>
         )}
-        <span className="font-medium text-gray-700 dark:text-gray-200">
+        <span className="font-medium text-gray-200">
           {currentPage} of {totalPages}
         </span>
         {!nextPage && (
-          <button className="px-4 py-2 text-gray-400 border rounded cursor-not-allowed opacity-50" disabled={!nextPage}>
+          <button className="px-4 py-2 text-gray-400 border rounded cursor-not-allowed opacity-50 border-gray-700" disabled={!nextPage}>
             Next
           </button>
         )}
@@ -61,7 +61,7 @@ function Pagination({ totalPages, currentPage }: PaginationProps) {
           <Link 
             href={`/${basePath}/page/${currentPage + 1}`} 
             rel="next"
-            className="px-4 py-2 text-gray-700 bg-white border rounded hover:bg-gray-100 transition-colors duration-200 dark:bg-gray-800 dark:text-gray-200 dark:hover:bg-gray-700 dark:border-gray-700"
+            className="px-4 py-2 border rounded transition-colors duration-200 bg-cyan-800 text-gray-200 hover:bg-cyan-300 border-cyan-500"
           >
             Next
           </Link>
@@ -87,23 +87,23 @@ export default function ListLayoutWithTags({
   return (
     <>
       <div className="container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="py-8 border-b border-gray-200 dark:border-gray-700">
-          <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl md:text-5xl">
+        <div className="py-8 border-b border-gray-700">
+          <h1 className="text-3xl font-extrabold tracking-tight text-gray-100 sm:text-4xl md:text-5xl">
             {title}
           </h1>
         </div>
         
         <div className="mt-6 flex flex-col sm:flex-row gap-8">
           {/* Sidebar */}
-          <div className="sm:block w-half sm:w-72 flex-shrink-0 sticky top-8 self-start ">
-            <div className="rounded-lg bg-gray-50 shadow-md dark:bg-gray-800 dark:shadow-gray-700/40 overflow-hidden">
+          <div className="sm:block w-half sm:w-72 flex-shrink-0 sticky top-8 self-start">
+            <div className="rounded-lg shadow-md bg-cyan-500 shadow-gray-700/40 overflow-hidden py-2">
               <div className="px-6 py-5">
                 {pathname.startsWith('/tags') ? (
                   <h3 className="text-primary-500 font-bold uppercase mb-4">All Posts</h3>
                 ) : (
                   <Link
                     href={`/tags`}
-                    className="font-bold text-gray-700 uppercase dark:text-gray-300 hover:text-primary-500 dark:hover:text-primary-500 mb-4 block"
+                    className="font-bolduppercase text-gray-300 hover:text-primary-500 hover:text-primary-500 mb-4 block"
                   >
                     All Posts
                   </Link>
@@ -113,13 +113,13 @@ export default function ListLayoutWithTags({
                     return (
                       <li key={t}>
                         {decodeURI(pathname.split('/tags/')[1]) === slug(t) ? (
-                          <span className="text-primary-500 block px-3 py-2 text-sm font-bold uppercase rounded bg-primary-50 dark:bg-primary-900/20">
+                          <span className="text-primary-500 block px-3 py-2 text-sm font-bold uppercase rounded bg-primary-50 bg-primary-900/20">
                             {`${t} (${tagCounts[t]})`}
                           </span>
                         ) : (
                           <Link
                             href={`/tags/${slug(t)}`}
-                            className="block px-3 py-2 text-sm font-medium text-gray-600 uppercase dark:text-gray-300 hover:text-primary-500 dark:hover:text-primary-500 hover:bg-gray-100 dark:hover:bg-gray-700/30 rounded transition-colors"
+                            className="block px-3 py-2 text-sm font-medium uppercase text-gray-300 hover:text-primary-500 hover:text-primary-500 hover:bg-gray-100 hover:bg-gray-700/30 rounded transition-colors"
                             aria-label={`View posts tagged ${t}`}
                           >
                             {`${t} (${tagCounts[t]})`}
@@ -135,16 +135,16 @@ export default function ListLayoutWithTags({
           
           {/* Main content */}
           <div className="flex-grow">
-            <ul className="divide-y divide-gray-700">
+            <ul className="divide-y">
               {displayPosts.map((post) => {
                 const { path, date, title, summary, tags } = post
                 return (
-                  <li key={path} className="py-6">
+                  <li key={path} className="py-4">
                     <article>
                       <div className="space-y-4 py-2">
                         <dl>
                           <dt className="sr-only">Published on</dt>
-                          <dd className="text-sm font-medium text-gray-500 dark:text-gray-400">
+                          <dd className="text-sm font-medium text-gray-400">
                             <time dateTime={date} suppressHydrationWarning>
                               {formatDate(date, 'en-EN')}
                             </time>
@@ -152,7 +152,7 @@ export default function ListLayoutWithTags({
                         </dl>
                         <div>
                           <h2 className="text-2xl font-bold tracking-tight mb-3">
-                            <Link href={`/${path}`} className="text-gray-900 dark:text-gray-100 hover:text-primary-600 dark:hover:text-primary-400 transition-colors">
+                            <Link href={`/${path}`} className=" text-gray-100 hover:text-primary-600 hover:text-primary-400 transition-colors">
                               {title}
                             </Link>
                           </h2>
@@ -162,7 +162,7 @@ export default function ListLayoutWithTags({
                             </div>
                           )}
                         </div>
-                        <div className="prose max-w-none text-gray-600 dark:text-gray-400">
+                        <div className="prose max-w-none text-gray-400">
                           {summary}
                         </div>
                       </div>
